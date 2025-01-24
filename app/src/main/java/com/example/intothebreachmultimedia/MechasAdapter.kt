1df966793f0d6b02771e20c1c6a150ead970d23e
Helio Verdugo <hverdugob2425@politecnics.barcelona>
2025-01-24 17:50:25 +0100

    Recycler View Hecho

---
+++ app/src/main/java/com/example/intothebreachmultimedia/MechasAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.intothebreachmultimedia
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+
+import androidx.recyclerview.widget.RecyclerView
+class MechasAdapter(private val mechaList: List<Mecha>,
+                    private val onItemClick: (Mecha) -> Unit
+                    ) : RecyclerView.Adapter<MechasAdapter.MechaViewHolder>() {
+
+    // ViewHolder interno que gestiona cada elemento de la lista
+    inner class MechaViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val nameTextView: TextView = view.findViewById(R.id.TxtMechName)
+        val imgMech: LinearLayout = view.findViewById(R.id.LayoutImg)
+        fun bind(mecha: Mecha) {
+            nameTextView.text = mecha.nameMecha
+            imgMech.setBackgroundResource(mecha.imgMecha)
+            // Evento al hacer clic en el elemento
+            itemView.setOnClickListener {
+                onItemClick(mecha)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MechaViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_mecha, parent, false)
+        return MechaViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MechaViewHolder, position: Int) {
+        val mecha = mechaList[position]
+        holder.bind(mecha)
+    }
+
+    override fun getItemCount(): Int {
+        return mechaList.size
+    }
+}
--- app/src/main/java/com/example/intothebreachmultimedia/ShowMechasActivity.kt
+++ app/src/main/java/com/example/intothebreachmultimedia/ShowMechasActivity.kt
@@ -2,8 +2,10 @@ package com.example.intothebreachmultimedia
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class ShowMechasActivity: AppCompatActivity() {
@@ -11,11 +13,26 @@ class ShowMechasActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_show_mechas)
         window.decorView.systemUiVisibility =
             (View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
 
         val rvMechas = findViewById<RecyclerView>(R.id.RVMechas)
 
+        val mechaList = listOf(Mecha("No se x1",R.drawable.into_the_breach_img),
+                                Mecha("No se x2",R.drawable.into_the_breach_img),
+                                Mecha("No se x3",R.drawable.into_the_breach_img),
+                                Mecha("No se x4",R.drawable.into_the_breach_img),
+                                Mecha("No se x5",R.drawable.into_the_breach_img),
+                                Mecha("No se x6",R.drawable.into_the_breach_img))
+
+
+
+        val adapter = MechasAdapter(mechaList) { selectedMecha ->
+            // Manejar clics en cada elemento
+            Toast.makeText(this, "Seleccionaste: ${selectedMecha.nameMecha}", Toast.LENGTH_SHORT).show()
+        }
+        rvMechas.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        rvMechas.adapter = adapter
     }
 }
